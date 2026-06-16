e9c54db914b751d97e2c721aec4ce6d4c175b16c
bharat246 <bharatsh246@gmail.com>
2026-06-16 14:46:04 +0530

    Update ForgotPassword.js

--- frontend/src/pages/ForgotPassword.js
+++ frontend/src/pages/ForgotPassword.js
@@ -1,41 +1,65 @@
-import axios from "axios";
 import { useState } from "react";
 
 function ForgotPassword() {
-
   const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    await axios.post(
-      "http://localhost:5000/api/auth/forgot-password",
-      { email }
+    setMessage(
+      "Password reset link has been sent to your email."
     );
-
-    alert("Email Sent");
   };
 
   return (
-    <div className="container mt-5">
+    <div className="container vh-100 d-flex justify-content-center align-items-center">
+      <div
+        className="card shadow-lg border-0 p-4"
+        style={{ width: "450px" }}
+      >
+        <div className="text-center mb-3">
+          <h2 className="fw-bold">
+            Forgot Password
+          </h2>
 
-      <h2>Forgot Password</h2>
+          <p className="text-muted">
+            Enter your registered email
+          </p>
+        </div>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          className="form-control"
-          placeholder="Email"
-          onChange={(e)=>
-          setEmail(e.target.value)}
-        />
+        {message && (
+          <div className="alert alert-success">
+            {message}
+          </div>
+        )}
 
-        <button
-          className="btn btn-primary mt-3">
-          Send Link
-        </button>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label className="form-label">
+              Email Address
+            </label>
 
-      </form>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="example@gmail.com"
+              value={email}
+              onChange={(e) =>
+                setEmail(e.target.value)
+              }
+              required
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="btn btn-primary w-100"
+          >
+            Send Reset Link
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
